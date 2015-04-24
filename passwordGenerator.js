b05cc9b64b2914825e9f7809d18598739dd01573
zerogravit1 <nograviti@gmail.com>
2015-04-23 20:27:12 -0400

    corrected formatting

--- passwordGenerator.js
+++ passwordGenerator.js
@@ -4,13 +4,12 @@ function makePasswords() {
   var text = "";
   var possible = "!#$%&*-0123456789@ABCDEFGHIJKLMNOPQRSTUVWXYZ^abcdefghijklmnopqrstuvwxyz";
 
-for(var i = 0; i < possible.length; ++i){
-  for(var j = 0; j < 8; ++j) {
-    text += possible.charAt(i);
-  }
-  console.log(text);
-}
-
+    for(var i = 0; i < possible.length; ++i){
+        for(var j = 0; j < 8; ++j) {
+            text += possible.charAt(i);
+        }
+    console.log(text);
+    }
 }
 
 makePasswords();
