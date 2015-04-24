61959907b4abc7742829c315f498442b6ef62ae5
zerogravit1 <nograviti@gmail.com>
2015-04-24 00:28:16 -0400

    increment right to left - need more RAM

--- passwordGenerator.js
+++ passwordGenerator.js
@@ -3,7 +3,8 @@
 function makePasswords() {
     var text = [];
     var hold = "";
-    var possible = "!#$%&*-0123456789@ABCDEFGHIJKLMNOPQRSTUVWXYZ^abcdefghijklmnopqrstuvwxyz";
+    var possibleOld = "!#$%&*-0123456789@ABCDEFGHIJKLMNOPQRSTUVWXYZ^abcdefghijklmnopqrstuvwxyz";
+    var possible = "abcdefghi"
     
     for (var i = 0; i < possible.length; ++i) {
         for (var j = 0; j < possible.length; ++j) {
@@ -13,18 +14,16 @@ function makePasswords() {
                         for (var n = 0; n < possible.length; ++n) {
                             for (var o = 0; o < possible.length; ++o) {
                                 for (var p = 0; p < possible.length; ++p) {
-                                    for (var q = 0; q < possible.length; ++q) {
-                                        hold += possible.charAt(0 + q);
-                                        hold += possible.charAt(0 + p);
-                                        hold += possible.charAt(0 + o);
-                                        hold += possible.charAt(0 + n);
-                                        hold += possible.charAt(0 + m);
-                                        hold += possible.charAt(0 + l);
-                                        hold += possible.charAt(0 + k);
+                                        hold += possible.charAt(0 + i);
                                         hold += possible.charAt(0 + j);
-                                        text.push(hold);
+                                        hold += possible.charAt(0 + k);
+                                        hold += possible.charAt(0 + l);
+                                        hold += possible.charAt(0 + m);
+                                        hold += possible.charAt(0 + n);
+                                        hold += possible.charAt(0 + o);
+                                        hold += possible.charAt(0 + p);
+                                        console.log(hold);
                                         hold = "";
-                                    }
                                 }
                             }
                         }
@@ -33,8 +32,6 @@ function makePasswords() {
             }
         }
     }
-
-    console.log(text);
 }
 
 makePasswords();
